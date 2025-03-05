fbeb6001ab6b69d76462f6ded91fedfe0bd50698
bachnvse <Bachnvhe172297@fpt.edu.vn>
2025-03-06 00:02:00 +0700

    add update

--- app/src/main/java/com/example/f_food/Screen/features_customer/FoodDetailActivity.java
+++ app/src/main/java/com/example/f_food/Screen/features_customer/FoodDetailActivity.java
@@ -1,6 +1,11 @@
 package com.example.f_food.Screen.features_customer;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.f_food.Entity.Food;
 import com.example.f_food.R;
-
+import com.example.f_food.Repository.FoodRepository;
+import com.squareup.picasso.Picasso;
+import java.text.NumberFormat;
+import java.util.Locale;
 public class FoodDetailActivity extends AppCompatActivity {
+
+    private FoodRepository foodRepository;
+
+    TextView dishName;
+    TextView price;
+
+    TextView description;
+
+    ImageView dishImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,31 @@ public class FoodDetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+        init();
+    }
+
+    private void init() {
+        dishName = findViewById(R.id.dishName);
+        price = findViewById(R.id.price);
+        description = findViewById(R.id.description);
+        dishImage = findViewById(R.id.dishImage);
+        Intent intent = getIntent();
+        foodRepository = new FoodRepository(this);
+        Food food = foodRepository.getFoodById(intent.getIntExtra("foodId", -1));
+        dishName.setText(food.getName());
+        NumberFormat formatter = NumberFormat.getInstance(new Locale("vi", "VN"));
+        price.setText("Giá tiền: " + formatter.format(food.getPrice()) + " VNĐ");
+        description.setText(food.getDescription());
+        if(food.getImageUrl() != null && !food.getImageUrl().isEmpty()) {
+            Picasso.get()
+                    .load(food.getImageUrl())
+                    .resize(500, 500)
+                    .centerCrop()
+                    .into(dishImage);
+        }
     }
 }
