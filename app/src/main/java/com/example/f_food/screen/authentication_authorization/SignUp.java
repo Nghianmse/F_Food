55da3c5ca4cbcca85c07514d7582d4d1d6e9ec55
TungCT <tungcthe176530@fpt.edu.vn>
2025-03-24 18:54:37 +0700

    update

--- app/src/main/java/com/example/f_food/screen/authentication_authorization/SignUp.java
+++ app/src/main/java/com/example/f_food/screen/authentication_authorization/SignUp.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -25,10 +24,9 @@ import com.squareup.picasso.Picasso;
 
 public class SignUp extends AppCompatActivity {
     private EditText fullName, email, phone, password, confirmPassword;
-    private Button btnSignUp;
+    private Button btnSignUp, signIn;
     private UserRepository userRepository;
-    private Button Signin;
-    ImageView imgLogoSignup;
+    private ImageView imgLogoSignup;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -49,72 +47,93 @@ public class SignUp extends AppCompatActivity {
         password = findViewById(R.id.txtSignup_Password);
         confirmPassword = findViewById(R.id.txtSignup_ConfirmPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
-        Signin = findViewById(R.id.btn_signup_login);
+        signIn = findViewById(R.id.btn_signup_login);
         imgLogoSignup = findViewById(R.id.imgLogoSignup);
+
         Picasso.get()
                 .load(R.drawable.login)
                 .resize(500, 500)
                 .centerCrop()
                 .into(imgLogoSignup);
-        Signin.setOnClickListener(v->{
+
+        signIn.setOnClickListener(v -> {
             Intent intent = new Intent(SignUp.this, LoginActivity.class);
             startActivity(intent);
         });
-        btnSignUp.setOnClickListener(v -> {
 
-            String name = fullName.getText().toString().trim();
-            String emailText = email.getText().toString().trim();
-            String phoneText = phone.getText().toString().trim();
-            String pass = password.getText().toString().trim();
-            String confirmPass = confirmPassword.getText().toString().trim();
-            userRepository = new UserRepository(this);
-
-            if (name.isEmpty() || emailText.isEmpty() || phoneText.isEmpty() || pass.isEmpty() || confirmPass.isEmpty()) {
-                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (!pass.equals(confirmPass)) {
-                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Kiểm tra xem email hoặc số điện thoại đã tồn tại chưa
-            if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
-                Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (userRepository.getUserByEmail(emailText) != null) {
-                Toast.makeText(this, "Email already exists", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            if (userRepository.getUserByPhone(phoneText) != null) {
-                Toast.makeText(this, "Phone number already exists", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Tạo đối tượng User mới và thêm vào cơ sở dữ liệu
-            User newUser = new User(0, name, emailText, pass, phoneText, "Customer", "2024-03-14 12:00:00", "2024-03-14 12:00:00",false);
-            // Show success message with AlertDialog
-            userRepository.insert(newUser);
-            AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
-            builder.setMessage("Registration successful")
-                    .setCancelable(false)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // Transition to login screen
-                            startActivity(new Intent(SignUp.this, LoginActivity.class));
-                            finish();
-                        }
-                    });
-
-            AlertDialog alert = builder.create();
-            alert.show();
-
-        });
+        btnSignUp.setOnClickListener(v -> handleSignUp());
     }
 
+    private void handleSignUp() {
+        String name = fullName.getText().toString().trim();
+        String emailText = email.getText().toString().trim();
+        String phoneText = phone.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        String confirmPass = confirmPassword.getText().toString().trim();
+        userRepository = new UserRepository(this);
 
+        if (name.isEmpty() || emailText.isEmpty() || phoneText.isEmpty() || pass.isEmpty() || confirmPass.isEmpty()) {
+            showToast("Please fill all fields.");
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            showToast("Invalid email format.");
+            return;
+        }
+
+        if (!isValidVietnamesePhoneNumber(phoneText)) {
+            showToast("Invalid Vietnamese phone number.");
+            return;
+        }
+
+        if (!isValidPassword(pass)) {
+            showToast("Password must be at least 8 characters long and start with an uppercase letter.");
+            return;
+        }
+
+        if (!pass.equals(confirmPass)) {
+            showToast("Passwords do not match.");
+            return;
+        }
+
+        if (userRepository.getUserByEmail(emailText) != null) {
+            showToast("Email already exists.");
+            return;
+        }
+
+        if (userRepository.getUserByPhone(phoneText) != null) {
+            showToast("Phone number already exists.");
+            return;
+        }
+
+        User newUser = new User(0, name, emailText, pass, phoneText, "Customer", "2024-03-14 12:00:00", "2024-03-14 12:00:00", false);
+        userRepository.insert(newUser);
+
+        showSuccessDialog();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showSuccessDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+        builder.setMessage("Registration successful")
+                .setCancelable(false)
+                .setPositiveButton("OK", (dialog, id) -> {
+                    startActivity(new Intent(SignUp.this, LoginActivity.class));
+                    finish();
+                });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    private boolean isValidPassword(String password) {
+        return password.length() >= 8 && Character.isUpperCase(password.charAt(0));
+    }
+
+    private boolean isValidVietnamesePhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("^0[0-9]{9,10}$");
+    }
 }
